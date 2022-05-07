ab574aed0d8192f577125dc17da452977fa4bdac
RAFAEL MASSARO <rafael.massaro@academico.unilasalle.edu.br>
2022-05-07 10:08:39 -0300

    Set Environment Variables

--- backend/src/use-cases/submit-feedback-use-case.ts
+++ backend/src/use-cases/submit-feedback-use-case.ts
@@ -40,6 +40,7 @@ export class SubmitFeedbackUseCase{
                 `<div style="font-size: sans-serif; font-size: 16px; color: #111; ">`,
                 `<p>Tipo do feedback: ${type}</p>`,
                 `<p>Comentário: ${comment}</p>`,
+                screenshot ? `<img src="${screenshot}"/>` : '',
                 `</div>`
             ].join('\n')
         })
--- web/src/components/WidgetForm/Steps/FeedBackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedBackContentStep.tsx
@@ -1,7 +1,9 @@
 import { ArrowLeft, Camera } from "phosphor-react";
 import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from ".."
+import { api } from "../../../lib/api";
 import { CloseButton } from "../../CloseButton"
+import { Loading } from "../../Loading";
 import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps{
@@ -17,14 +19,36 @@ export function FeedbackContentStep({
 }: FeedbackContentStepProps){
     const [screenshot, setScreenshot] = useState<string | null>(null)
     const [comment, setComment] = useState('')
+    const [isSendingFeedback, setIsSendingFeedback] = useState(false)
 
 
     const feedbackTypeInfo = feedbackTypes[feedbackType]
 
-    function handleSubmitfeedback(event:FormEvent){
+    async function handleSubmitfeedback(event:FormEvent){
         event.preventDefault()
         
+        setIsSendingFeedback(true)
+
         
+
+        try{
+            await api.post('/feedbacks', {
+                type: feedbackType,
+                comment,
+                screenshot,
+            })
+
+            console.log({
+                type: feedbackType,
+                comment,
+                screenshot,
+            })
+
+        }catch(err){
+            console.log(err)
+        }
+        
+        setIsSendingFeedback(false)
         onFeedbackSend()
     }
 
@@ -62,10 +86,17 @@ export function FeedbackContentStep({
 
                     <button
                         type="submit"
-                        disabled={comment.length === 0}
+                        disabled={comment.length === 0 || isSendingFeedback}
                         className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
                     >
-                        Enviar feedback
+                        {
+                            isSendingFeedback 
+                                ? 
+                                    <Loading />
+                                : 
+
+                                'Enviar feedback'
+                        }
                     </button>
                 </footer>
             </form>
