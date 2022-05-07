334037f94fe11041cbe1dfe9aec16a82f4f9bac0
Rafael <rafaelmassaro9@gmail.com>
2022-05-07 19:33:02 -0300

    Chaged server PORT

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,4 +9,6 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333, () => console.log('Server running at port 3333'))
+app.listen( process.env.PORT || 3333, () => {
+    console.log('Server running at port 3333')
+})
